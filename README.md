2c5fe5551382d5bd37debe00b1ebac420a69e8d6
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-27 19:12:26 +0100

    Created basic code structure, works

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Aufgabe12
 Programmieraufgabe 12 vom Java Masterkurs auf programmieren-starten.de
+
+Ein kleiner Speicher fuer Ganzzahlen
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-         int[][] field = {{1, 2, 3, 4, 5, 6},
-                          {7, 8, 9, 10, 11, 12},
-                          {13, 14, 15, 16, 17, 18},
-                          {19, 20, 21, 22, 23, 24}};
-
-         //System.out.println(field.length);
-         //System.out.println(field[3].length);
-
-         for(int i = 0; i < field.length; i++) {
-             for(int j = 0; j < field[i].length; j++) {
-                 System.out.print(field[i][j] + ", ");
-             }
-             System.out.println();
-         }
+        int num, high;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Wie viele Zahlen moechten Sie abspeichern? Bitte eingeben:");
+        num = sc.nextInt();
+        int[] array = new int[num];
+        System.out.println("Sie haben eine Speicherkapazitaet von " + num + " Zahlen ausgewaehlt.");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Bitte geben sie jetzt Zahl Nr.: " + (i + 1) + " ein: ");
+            array[i] = sc.nextInt();
+        }
+        System.out.print("Sie haben folgende Zahlen eingegeben: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println();
+        high = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > high) {
+                high = array[i];
+            }
+        }
+        System.out.println("Die groesste der eingelesenen Zahlen ist: " + high);
 
     }
+
 }
